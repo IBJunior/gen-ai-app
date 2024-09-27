cd8803a04748970854bb54f6b56f5b08fe9680a3
Ali ibrahim <iali96369@gmail.com>
2024-09-27 22:11:39 +0200

    Refactoring

--- src/app/services/assistant-service/assistant-service.service.ts
+++ src/app/services/assistant-service/assistant-service.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AssistantService {
-  private apiUrl = 'http://localhost:8080/api/assistant/faq';
+  private apiUrl = 'http://localhost:8080/api/assistant/destinations';
 
 
   getDataStream(userInput: string): Observable<any> {
